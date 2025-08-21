8704cfe9b6e1f41e3cfb3952494ce37d9a703c88
Marcos Mora <abreu760@hotmail.com>
2025-08-21 23:09:32 +0000

    u

--- src/crozz-coin/src/blockchain/blockchain.ts
+++ src/crozz-coin/src/blockchain/blockchain.ts
@@ -1,2 +1,3 @@
-import Wallet from './wallet/wallet';
-import Blockchain from './blockchain/blockchain';
+// Update the import path if necessary, or comment out if the file does not exist
+// import Wallet from './wallet/wallet';
+// import Blockchain from './blockchain/blockchain';
